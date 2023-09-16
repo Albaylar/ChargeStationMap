dec5c3152ea9563ae6f51b61c76497da4ccfe7b3
Albaylar <furkandenizalbaylar@gmail.com>
2023-09-17 00:49:30 +0300

    Initial Commit

---
+++ ChargeStationMap/ChargeStationMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeStationMapApp.swift
+//  ChargeStationMap
+//
+//  Created by Furkan Deniz Albaylar on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChargeStationMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
